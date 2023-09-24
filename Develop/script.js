09473bc4fe805f584668ba2d8a8012b845b1032d
Osmilliardo <140178301+Osmilliardo@users.noreply.github.com>
2023-09-24 18:52:28 -0400

    finished loop to check if divs contain stored data and add it appropriately, also cleaned up some of the instruction code

--- Develop/script.js
+++ Develop/script.js
@@ -5,43 +5,25 @@ $(document).ready()
 
 renderSchedule()
 
-// $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  $('.saveBtn').on('click',function() {
-      // console.log($(this).parent().attr('id'))
-      // console.log($(this).siblings('textarea').val())
-      localStorage.setItem($(this).parent().attr('id'), $(this).siblings('textarea').val())
-  })
 
-  function renderSchedule() {
-    var hourClass = ['hour-9', 'hour-10', 'hour-11', 'hour-12', 'hour-13', 'hour-14', 'hour-15', 'hour-16', 'hour-17']
-    console.log(localStorage.getItem(hourClass[2]))
-    console.log(hourClass.length)
-    for(i = 0; i < hourClass.length; i++) {
-      // console.log(hourClass[i])
-      if (localStorage.getItem(hourClass[i]) !== '')
-        console.log(localStorage.getItem(hourClass[i]))
-    } 
-    
+$('.saveBtn').on('click',function() {
+    // console.log($(this).parent().attr('id'))
+    // console.log($(this).siblings('textarea').val())
+    localStorage.setItem($(this).parent().attr('id'), $(this).siblings('textarea').val())
+})
+
+function renderSchedule() {
+  var hourID = ['hour-9', 'hour-10', 'hour-11', 'hour-12', 'hour-13', 'hour-14', 'hour-15', 'hour-16', 'hour-17'] //array to establish keys according to the div id's
+  // console.log(localStorage.getItem(hourClass[2]))
+  for(i = 0; i < hourID.length; i++) {
+  // console.log(hourID[i])
+  if (localStorage.getItem(hourID[i])) { //checks that the key has a value before adding any text
+    // console.log(localStorage.getItem(hourID[i]))
+    // console.log($(document.body.children[1].children[hourID[i]]))
+    $(document.body.children[1].children[i].children[1]).text(localStorage.getItem(hourID[i])) //add the appropriate localStorage text to the appropriate key/div
+    }
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
